c875745b1f1bbc2479934dc70ffb31c3f1c78476
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2025-05-15 23:01:49 -0500

    desing event by id

--- src/app/(public)/challenges/page.tsx
+++ src/app/(public)/challenges/page.tsx
@@ -1,5 +1,9 @@
 import ChallengeShowcaseServer from "../../../../Components/challenge/ChallengeShowcaseServer";
 
 export default function ChallengesPage() {
-  return <ChallengeShowcaseServer />;
+  return (
+    <div className="min-h-screen bg-[#0F0F0F]">
+      <ChallengeShowcaseServer />
+    </div>
+  );
 }
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,7 +3,7 @@ import Hero from "../../../Components/index/Hero";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-[#0F0F0F]">
+    <div className="min-h-screen bg-black">
       <Hero />
     </div>
   );
