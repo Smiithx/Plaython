be3562dd5fbfa05984690a530b25c5e869af53ee
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2025-05-18 14:39:43 -0500

    add details design dashboard

--- Components/challenge/id/ChallengeDetailClient.tsx
+++ Components/challenge/id/ChallengeDetailClient.tsx
@@ -12,12 +12,19 @@ import {
   ChevronLeft,
   Share2,
   Lock,
+  ArrowLeft,
 } from "lucide-react";
 import { Badge } from "@/ui/badge";
 import Link from "next/link";
 import { Button } from "@/ui/button";
 import StarField from "@/ui/animations/star-footer";
-import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 interface ChallengeDetailClientProps {
   eventData: Challenge;
@@ -68,25 +75,35 @@ export default function ChallengeDetailClient({
         <div className="absolute inset-0 opacity-30 mix-blend-overlay"></div>
         <div className="absolute inset-0 bg-gradient-to-t from-gray-950 to-transparent"></div>
         <div className="relative container mx-auto px-4 h-full flex flex-col justify-end pb-8">
-          {/* Botón de retroceso */}
-          <Link
-            href="/challenges"
-            className="z-10 inline-flex items-center absolute top-4 left-4 gap-2 px-4 py-2 rounded-lg bg-gray-800/50 hover:bg-gray-700/50 transition-all duration-300 text-gray-300"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-            Volver a retos
-          </Link>
+          <div className="z-10 inline-flex items-center justify-between p-4 text-white">
+            {/* Botón de retroceso */}
+            <div>
+              <Link
+                href="/challenges"
+                className="z-10 inline-flex items-center absolute top-4 left-4 gap-2 px-4 py-2 rounded-lg bg-gray-800/50 hover:bg-gray-700/50 transition-all duration-300 text-gray-300"
+              >
+                <ArrowLeft />
+                Volver a retos
+              </Link>
+            </div>
+
+            <div className="z-10 inline-flex items-center absolute top-4 right-4 gap-10 px-4 transition-all duration-300 text-gray-300">
+              <SignedIn>
+                <span className="border-b-2 border-transparent transition-colors duration-300 hover:bg-gradient-to-r hover:from-[#FF9A8B]/20 hover:via-[#5865F2]/20 hover:to-[#9146FF]/20 hover:border-white/40 hover:rounded-t-lg px-4 py-2">
+                  <Link
+                    href="/dashboard"
+                    className="font-[var(--font-bebas)] text-sm text-white/80 inline-block"
+                  >
+                    Panel
+                  </Link>
+                </span>
+                <div className="hidden md:flex items-center gap-2 bg-white/10 rounded-full px-3 py-1.5">
+                  <UserButton />
+                </div>
+              </SignedIn>
+            </div>
+          </div>
+
           <div className="flex flex-wrap items-center gap-3 mb-8">
             {eventData?.tags.map((category, index) => (
               <Badge
